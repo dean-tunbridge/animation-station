f23d684fe7e468d56a13b6867b68b7799ea7b75b
Dean Tunbridge <deantunbridgedev@gmail.com>
2025-03-17 17:02:25 +1300

    font still isnt loading

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,9 @@ import React from 'react'
 function App() {
   return (
     <main>
-      <h1 className="text-5xl text-orange-500 font-bold ">ANIMATION STATION</h1>
+      <h1 className="text-5xl text-orange-500 font-circular-web">
+        ANIMATION STATION
+      </h1>
     </main>
   )
 }
