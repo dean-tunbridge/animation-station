fe4f0e5337e2da06a5208c1a32a853b93502aed2
Dean Tunbridge <deantunbridgedev@gmail.com>
2025-03-17 17:27:26 +1300

    cant even import basic things into my app file so not sure what is going on at all

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,21 @@
+import About from './components/About'
+import Hero from './components/Hero'
+import NavBar from './components/Navbar'
+import Features from './components/Features'
+import Story from './components/Story'
+import Contact from './components/Contact'
+import Footer from './components/Footer'
+
 function App() {
   return (
     <main className="relative min-h-screen w-screen overflow-x-hidden">
-      <h1>TEST</h1>
+      <NavBar />
+      <Hero />
+      <About />
+      <Features />
+      <Story />
+      <Contact />
+      <Footer />
     </main>
   )
 }
